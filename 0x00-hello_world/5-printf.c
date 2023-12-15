7d8cf2060723ece958791a6ddc1b3c6c9d54392e
Nourhan97N <nourhan.elshatby@gmail.com>
2023-12-15 12:41:36 -0800

    00

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+   printf("Art:\n");
+   printf(" *****\n");
+   printf(" *   *\n");
+   printf("*     *\n");
+   printf(" *   *\n");
+   printf(" *****\n");
+   return 0;
+}
+
