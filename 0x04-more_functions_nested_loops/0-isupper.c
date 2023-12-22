b495ed0a245817f952e0dd0a85c842cb5488141c
Nourhan97N <nourhan.elshatby@gmail.com>
2023-12-22 02:09:07 -0800

    0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,1 @@
-#include <ctype.h>
-
-int _isupper(int c) {
-   return isupper(c);
-}
-gcc -o main main.c
-./main
-
+#include "main.h"/** * main - Prints _putchar followed by a newline * * Return: Always 0. */int main(void){int str[] = {95, 112, 117, 116, 99, 104, 97, 114};int count, sz;sz = sizeof(str) / sizeof(int);for (count = 0; count < sz; count++){ _putchar(str[count]);} _putchar('\n');return (0);
